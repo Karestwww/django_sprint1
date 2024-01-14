dd1ac84b48362faf8cad81f6b136dfadce50a552
Cheremisov Aleksey <karestwww@yandex.ru>
2024-01-14 21:22:41 +0400

    ENAF

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.index, name='index'),
     path('posts/<int:id>/', views.post_detail, name='post_detail'),
     path('category/<slug:category_slug>/', views.category_posts,
          name='category_posts'),
